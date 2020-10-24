78722629c44ede2b4d8ea0956657210b096576f9
ericson f <0000729834@senaimgaluno.com.br>
2020-10-23 21:48:09 -0300

    primeira versao

---
+++ src/br/com/fiemg/conexao/Conexao.java
@@ -0,0 +1,34 @@
+
+package br.com.fiemg.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class Conexao {
+    public static Connection conectar(){
+        Connection con = null;
+        final String URL = "jdbc:mysql://localhost:3306/fiemg?useTimezone=true&serverTimezone=UTC";
+        final String USER = "root";
+        final String PASS = "kim123456789";
+        
+        try {
+            con = DriverManager.getConnection(URL, USER, PASS);
+            return con;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+    public static void desconectar (Connection con)
+    {
+        try {
+            con.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Erro" +e);
+
+
+
+        }
+    }
+}
